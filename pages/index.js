be1d470235bbfa93cafd6dcd42902174fe5838b1
brettlockhart <brettlockhart@gmail.com>
2023-04-08 01:38:47 -0400

    errors fixed

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,10 @@ export default function Home() {
       <h1>My RSS Feeds</h1>
       <ul id="feed-list"></ul>
       <script
+        src="https://code.jquery.com/jquery-3.6.0.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+      <script
         src="/fetch-rss.js"
         type="module"></script>
       <script
